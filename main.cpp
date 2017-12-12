8bdd23708d02066c6d430c1a4f97fe45f35b038c
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2017-12-12 14:06:11 -0500

    feat: add principal files to test algorithms

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  
+  long long maxIterations;
+  long long iterations = 0;
+
+  cin >> maxIterations;
+
+  cout << maxIterations;
+  
+  return 0;
+}
