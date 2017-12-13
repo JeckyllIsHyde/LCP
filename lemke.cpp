911985996613b7a8f27b71e95c0b11e94eeb5af5
Jaime Andres <jacastillol@gmail.com>
2017-12-13 08:54:06 -0500

    feat: add LCP class

--- lemke.cpp
+++ lemke.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+struct LCP {
+  typedef std::vector<double> Array1D;
+  typedef std::vector< Array1D > Array2D;
+  Array1D q;
+  Array2D M;
+};
+
 int main() {
 
   // lemke's algorithm
