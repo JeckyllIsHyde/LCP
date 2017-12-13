3bcd4370c78d9cd2616d631c6dc786e480011c8f
Jaime Andres <jacastillol@gmail.com>
2017-12-13 09:31:47 -0500

    feat: Add and test read() method from cin stream

--- lemke.cpp
+++ lemke.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
+#include <vector>
 
 struct LCP {
   typedef std::vector<double> Array1D;
   typedef std::vector< Array1D > Array2D;
   Array1D q;
   Array2D M;
+
+  void read(void);
 };
 
 int main() {
 
   // lemke's algorithm
+  LCP problem;
+  
+  problem.read();
+
+  std::cout << problem.q.size() << std::endl;
+  for (int i=0; i<problem.q.size(); i++)
+    std::cout << problem.q[i] << " ";
+  std::cout << std::endl;
+  for (int i=0; i<problem.M.size(); i++) {
+    for (int j=0; j<problem.M[i].size(); j++)
+      std::cout << problem.M[i][j] << " ";
+    std::cout << std::endl;
+  }
   
   return 0;
+}
+
+void LCP::read() {
+  int n;
+  // problem size
+  std::cin >> n;
+  q.resize(n,0.0);
+  M.resize(n,Array1D(n,0.0));
+  // set q vector
+  for (int i=0; i<q.size(); i++)
+    std::cin >> q[i];
+  // set M vector
+  for (int i=0; i<M.size(); i++)
+    for (int j=0; j<M[i].size(); j++)
+      std::cin >> M[i][j];
 }
