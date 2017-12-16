b72d4662fd037c9986418d9ab33dca5c04955cae
Jaime Andres <jacastillol@gmail.com>
2017-12-16 11:12:43 -0500

    refactor: add Tableau class

--- lemke.cpp
+++ lemke.cpp
@@ -12,20 +12,27 @@ template<typename T> void print(T& v);
 void print(Array2D& m);
 void print(Array2UI& b);
 
+struct Tableau {
+  Array2UI basis;
+  Array2D data;
+  int q_col, z0_col, z0_row;
+  int n_rows, n_cols;
+
+  void print_status();
+  int find_initial_pivot();
+  int find_pivot(int col_p);
+  void reduce_from_with_pivot(int i, int row_p, int col_p);
+  void pivot_reduction(int row_p, int col_p);
+};
+
 struct LCP {
   Array1D q;
   Array2D M;
-  Array2D tableau;
 
   void read_LCP(void);
   void print_LCP(void);
-  void make_tableau();
-  void lemke_algorithm();
-
-  void reduce_from_with_pivot(int i, int row_p, int col_p);
-  void pivot_reduction(int& row_p, int& col_p);
-  void find_initial_pivot(int& row_p);
-  void find_pivot(int& row_p, int& col_p);
+  void make_tableau(Tableau& tableau);
+  void lemke_algorithm(void);
 };
 
 int main() {
@@ -84,38 +91,42 @@ void LCP::print_LCP() {
   print(M);
 }
 
-void LCP::make_tableau() {
-  // make lemke's tableau
+void LCP::make_tableau(Tableau& tableau) {
+  // make lemke's tableau and basis
   int n = q.size();
-  tableau.resize(n, Array1D(2*n+2,0.0));
+  tableau.data.resize(n, Array1D(2*n+2,0.0));
+  tableau.basis.resize( 2, Array1UI( n, 0.0) );
   for (int i=0; i<n; i++) {
-    tableau[i][i] = 1.0;
-    tableau[i][2*n] = -1.0;
-    tableau[i][2*n+1] = q[i];
+    // data
+    tableau.n_rows = n;
+    tableau.n_cols = 2*n+2;
+    tableau.z0_col = 2*n;
+    tableau.q_col = 2*n+1;
+    tableau.data[i][i] = 1.0;
+    tableau.data[i][2*n] = -1.0;
+    tableau.data[i][2*n+1] = q[i];
+    // basis
+    tableau.basis[0][i]=0.0; tableau.basis[1][i]=i+1.0;
   }
   for (int i=0; i<n; i++)
     for (int j=0; j<n; j++)
-      tableau[i][j+n] = M[i][j];
+      tableau.data[i][j+n] = M[i][j];
 }
 
 void LCP::lemke_algorithm() {
-  make_tableau();
+
+  Tableau tableau;
+
+  make_tableau(tableau);
 
   std::cout << "INITIAL TABLEAU: " << std::endl;
+  tableau.print_status();
 
-  int n = tableau.size();
-  // create and init basis indicator
-  Array2UI basis( 2, Array1UI( n, 0.0) );
-  for (int i=0; i<n; i++) {
-    basis[0][i]=0.0; basis[1][i]=i+1.0;
-  }
-  std::cout << "basis:\n";  print(basis);
-  print(tableau);
-  int col_z0=2*n, row_z0, row_p;
-
+  int row_p;
+  g
   // step 1.
   // find initial row pivot
-  find_initial_pivot(row_p);
+  row_p = tableau.find_initial_pivot();
   // std::cout << qmin << std::endl;
   if (row_p==-1) {
     std::cout << "Find Solution!!!\nz = ";
@@ -124,56 +135,59 @@ void LCP::lemke_algorithm() {
   }
   // z0 -> Basis and basis -> wi
   int row_p_old;
-  basis[0][row_p] = 1;
-  basis[1][row_p] = row_z0 = row_p;
+  tableau.basis[0][row_p] = 1;
+  tableau.basis[1][row_p] = row_p;
   row_p_old = row_p;
-  int col_p = col_z0;
+  int col_p = tableau.z0_row;
 
   // step 2. row operations in idx-row to enter basis
-  pivot_reduction( row_p, col_z0 );
-  row_z0 = row_p_old = row_p;
+  tableau.pivot_reduction( tableau.z0_row, tableau.z0_col );
+  row_p_old = row_p;
 
   std::cout << "TABLEAU " << 1 << std::endl;
-  std::cout << "basis:\n";  print(basis);
-  print(tableau);
+  std::cout << "basis:\n";  print(tableau.basis);
+  print(tableau.data);
 
-  int counter=2, max_iter=3;
+  int counter=2, max_iter=2;
   do {
     // step 3. with row_p_old exit find complement enter the basis
 
     // step 4.
     //    row_p_old = row_p;
-    find_pivot( row_p, col_p );
+    row_p = tableau.find_pivot( col_p );
     if (row_p==-1) {
       std::cout << "Ray Solution!!!\n";
       return;
     }
 
     // step 5.
-    pivot_reduction(row_p,col_p);
+    tableau.pivot_reduction(row_p,col_p);
     std::cout << "TABLEAU " << counter << std::endl;
-    std::cout << "basis:\n"; print(basis);
-    print(tableau);
+    tableau.print_status();
   } while(counter++<max_iter);
 }
 
-void LCP::reduce_from_with_pivot(int row_i, int row_p, int col_p) {
-  int n = tableau.size();
+void Tableau::print_status() {
+  std::cout << "basis:\n"; print(basis);
+  print(data);
+}
+
+void Tableau::reduce_from_with_pivot(int row_i, int row_p, int col_p) {
+  int n = data.size();
   int m = 2*n+2;
-  double pivot = tableau[row_p][col_p];
+  double pivot = data[row_p][col_p];
   if (row_i==row_p)
-    for (int j=0; j<m; j++)
-      tableau[row_p][j]/=pivot;
+    for (int j=0; j<n_cols; j++)
+      data[row_p][j]/=pivot;
   else {
-    double bij=tableau[row_i][col_p]/pivot;
-    for (int j=0; j<m; j++)
-      tableau[row_i][j]-=tableau[row_p][j]*bij;
+    double bij=data[row_i][col_p]/pivot;
+    for (int j=0; j<n_cols; j++)
+      data[row_i][j]-=data[row_p][j]*bij;
   }
 }
 
-void LCP::pivot_reduction(int& row_p, int& col_p) {
-  int n = tableau.size();
-  for (int i=0; i<n; i++) {
+void Tableau::pivot_reduction(int row_p, int col_p) {
+  for (int i=0; i<n_rows; i++) {
     if (i==row_p)
       continue;
     reduce_from_with_pivot(i,row_p,col_p);
@@ -181,25 +195,26 @@ void LCP::pivot_reduction(int& row_p, int& col_p) {
   reduce_from_with_pivot(row_p,row_p,col_p);
 }
 
-void LCP::find_pivot(int& row_p, int& col_p) {
-  int n = tableau.size();
-  row_p = -1;
+int Tableau::find_pivot(int col_p) {
+  int row_p = -1;
   double rmin = 1e18;
-  for (int i=0; i<n; i++)
-    if (tableau[i][2*n+1]/tableau[i][col_p]<=rmin &&
-	0.0<tableau[i][col_p]) {
+  for (int i=0; i<n_rows; i++)
+    if (data[i][q_col]/data[i][col_p]<=rmin &&
+	0.0<data[i][col_p]) {
       row_p=i;
-      rmin=tableau[i][2*n+1]/tableau[i][col_p];
+      rmin=data[i][q_col]/data[i][col_p];
     }
+  return row_p;
 }
 
-void LCP::find_initial_pivot(int& row_p) {
-  row_p = -1;
+int Tableau::find_initial_pivot() {
+  int row_p = -1;
   double qmin = 1e18;
-  for (int i=0; i<q.size(); i++) // exist(qi<0) and argmin_i qi
-    if (q[i]<=qmin
-	&& q[i]<0.0) {
+  for (int i=0; i<n_rows; i++) // exist(qi<0) and argmin_i qi
+    if (data[i][q_col]<=qmin
+	&& data[i][q_col]<0.0) {
       row_p = i;
-      qmin = q[i];
+      qmin = data[i][q_col];
     }
+  return z0_row=row_p;
 }
